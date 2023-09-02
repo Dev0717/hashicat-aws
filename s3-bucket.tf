9b50aefee67386d6a3dc5fa775acca1d50f53587
Dev0717 <shanice.musiitwa08@gmail.com>
2023-09-01 20:23:07 -0400

    fixed error

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -5,16 +5,16 @@ module "s3-bucket" {
   bucket_prefix = "shanice-musiitwa"
 }
 
-curl \
-  --header "Authorization: Bearer $TOKEN" \
-  --header "Content-Type: application/vnd.api+json" \
-  --request POST \
-  --data @apply.json \
-  https://app.terraform.io/api/v2/workspaces/ws-4arCufdD2z46zHRf/vars
+# curl \
+#   --header "Authorization: Bearer $TOKEN" \
+#   --header "Content-Type: application/vnd.api+json" \
+#   --request POST \
+#   --data @apply.json \
+#   https://app.terraform.io/api/v2/workspaces/ws-4arCufdD2z46zHRf/vars
 
 
-  curl \
-  --header "Authorization: Bearer $TOKEN" \
-  --header "Content-Type: application/vnd.api+json" \
-"https://app.terraform.io/api/v2/workspaces/ws-4arCufdD2z46zHRf/vars"
+#   curl \
+#   --header "Authorization: Bearer $TOKEN" \
+#   --header "Content-Type: application/vnd.api+json" \
+# "https://app.terraform.io/api/v2/workspaces/ws-4arCufdD2z46zHRf/vars"
 
